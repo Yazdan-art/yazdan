d5c45bf2be46124420512bf1b05468995eb8cf9a
Yazdan <yazdan.ghezelbash@gmail.com>
2023-11-02 09:20:29 +0330

    part 1

---
+++ Desktop/VS code/str1='hellow'.py
@@ -0,0 +1,23 @@
+# str1='hellow'
+# str2="world"
+# bob =str1+str2
+# print(bob)
+# str3="123"
+# x=int(str3)+1
+# print(x)
+
+#___________________
+# age=int(input("enter your age"))
+# if(age>18):
+#     print("age >18")
+# elif(age==18):   
+#     print("age==18") 
+# else:
+#     print("age<18")
+#______________________
+fruit="banana"
+letter=fruit[1]
+print(letter)
+print("\n")
+x=3
+print(fruit[x-3])
